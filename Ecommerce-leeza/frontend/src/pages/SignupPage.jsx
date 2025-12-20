442faf7c74604416ff517351cda5a7209e1d1eea
leeza701 <leezamalik789@gmail.com>
2025-12-20 19:01:43 +0530

    deployment changes

--- Ecommerce-leeza/frontend/src/pages/SignupPage.jsx
+++ Ecommerce-leeza/frontend/src/pages/SignupPage.jsx
@@ -4,7 +4,7 @@ import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
 import { useUserStore } from "../stores/useUserStore";
 
-const SignUpPage = () => {
+const SignupPage = () => {
 	const [formData, setFormData] = useState({
 		name: "",
 		email: "",
@@ -157,4 +157,4 @@ const SignUpPage = () => {
 		</div>
 	);
 };
-export default SignUpPage;
+export default SignupPage;
