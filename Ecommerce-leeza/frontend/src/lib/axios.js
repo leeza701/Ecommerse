350378a3a1e4e041d0913705a2cd846d1bbc7946
leeza701 <leezamalik789@gmail.com>
2025-12-21 16:59:40 +0530

    deployment changes

--- Ecommerce-leeza/frontend/src/lib/axios.js
+++ Ecommerce-leeza/frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance=axios.create({
     //baseURL:import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
-    baseURL: "http://localhost:5001/api",
+    baseURL: "https://ecommerse-fullstack-79i2.onrender.com/api",
     withCredentials:true,
 })
 
