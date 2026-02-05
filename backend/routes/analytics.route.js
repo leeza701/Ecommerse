48a39f6a792000dedb452b2f1e42a2bdf3ee3f2d
leeza701 <leezamalik789@gmail.com>
2026-02-05 16:30:09 +0530

    solve analytics tab error

--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -1,25 +1,10 @@
 import express from "express";
 import { adminRoute ,protectRoute} from "../middleware/auth.middleware.js";
-import { getAnalyticsData , getDailySalesData} from "../controllers/analytics.controller.js";
+import { getAnalyticsData , getSalesData} from "../controllers/analytics.controller.js";
 
 const router=express.Router();
 
-router.get("/",protectRoute,adminRoute,async(req,res)=>{
-    try {
-        const analyticsData=await getAnalyticsData();
-
-        const endDate=new Date();
-        const startDate=new Date(endDate.getTime()-(7*24*60*60*1000));
-
-        const dailySalesData=await getDailySalesData(startDate,endDate);
-
-        res.json({analyticsData,dailySalesData,});
-
-
-    } catch (error) {
-        console.log("error in analytic route",error.message);
-        res.status(500).json({message:"server error",error: error.message});
-    }
-})
+router.get("/",protectRoute,adminRoute,getAnalyticsData)
+router.get("/sales" , protectRoute , adminRoute , getSalesData)
 
 export default router;
